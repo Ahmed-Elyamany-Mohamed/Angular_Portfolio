19a4d64c498c4cea6f159e99505f32b9f1c8478a
Ahmed-Elyamany-Mohamed <ahmedelyamany.mohamed@gmail.com>
2025-04-20 15:23:50 +0200

    add cv and fix some ui

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,14 +3,11 @@ import {
   importProvidersFrom,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { routes } from './app.routes';
-import { provideRouter } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
     importProvidersFrom(HttpClientModule), // ✅ Add this line
   ],
 };
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,11 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './skills.component.html',
-  styleUrl: './skills.component.css'
+  styleUrl: './skills.component.css',
 })
 export class SkillsComponent {
-
+  skills = [
+    { icon: 'fa-html5', color: 'rgb(253, 95, 32)', name: 'HTML5' },
+    { icon: 'fa-css3', color: 'rgb(78, 183, 248)', name: 'CSS' },
+    { icon: 'fa-js', color: 'rgb(233, 233, 13)', name: 'JavaScript' },
+    { icon: 'fa-angular', color: 'rgb(225, 18, 18)', name: 'Angular' },
+    { icon: 'fa-bootstrap', color: 'rgb(137, 39, 228)', name: 'BootStrap' },
+    { icon: 'fa-figma', color: 'blueviolet', name: 'Figma' },
+    { icon: 'fa-node-js', color: 'rgb(24, 163, 24)', name: 'Node Js' },
+    { icon: 'fa-github', color: 'rgb(137, 39, 228)', name: 'GitHub' },
+    { icon: 'fa-git-alt', color: 'rgb(255, 96, 33)', name: 'Git' },
+    { icon: 'fa-python', color: 'rgb(233, 233, 13)', name: 'Python' },
+  ];
 }
