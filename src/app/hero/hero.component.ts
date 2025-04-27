af946f659790c01cf237353fe9e97927984584d6
Ahmed-Elyamany-Mohamed <ahmedelyamany.mohamed@gmail.com>
2025-04-27 13:49:13 +0300

    added custom icons

--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { SafeLinkDirective } from '../directives/safe-link.directive';
 
 @Component({
@@ -7,8 +7,47 @@ import { SafeLinkDirective } from '../directives/safe-link.directive';
   templateUrl: './hero.component.html',
   styleUrl: './hero.component.css',
 })
-export class HeroComponent {
+export class HeroComponent implements OnInit {
   disableClick(event: MouseEvent) {
     event.preventDefault();
+  }
+  words: string[] = [' Developer.', ' Designer.', ' Creator.'];
+  currentWordIndex: number = 0;
+  currentCharIndex: number = 0;
+  isDeleting: boolean = false;
+
+  @ViewChild('animatedText', { static: true }) textElement!: ElementRef;
+
+  typeEffect(): void {
+    if (!this.textElement) return;
+
+    const currentWord: string = this.words[this.currentWordIndex];
+
+    if (this.isDeleting) {
+      this.currentCharIndex--;
+    } else {
+      this.currentCharIndex++;
+    }
+
+    this.textElement.nativeElement.textContent = currentWord.substring(
+      0,
+      this.currentCharIndex
+    );
+
+    if (!this.isDeleting && this.currentCharIndex === currentWord.length) {
+      this.isDeleting = true;
+      setTimeout(() => this.typeEffect(), 1000);
+      return;
+    } else if (this.isDeleting && this.currentCharIndex === 0) {
+      this.isDeleting = false;
+      this.currentWordIndex = (this.currentWordIndex + 1) % this.words.length;
+    }
+
+    const typingSpeed: number = this.isDeleting ? 100 : 150;
+    setTimeout(() => this.typeEffect(), typingSpeed);
+  }
+
+  ngOnInit(): void {
+    this.typeEffect();
   }
 }
